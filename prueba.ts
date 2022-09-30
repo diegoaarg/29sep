b485252ec44a778baa67eff045e1ac7bb14df96e
diegoaarg <114456613+diegoaarg@users.noreply.github.com>
2022-09-29 21:25:44 -0600

    Add files via upload

---
+++ prueba.ts
@@ -0,0 +1,8 @@
+let nombre: string="Diego"
+let number: number= 2
+let flag: boolean=false
+let cualquiera:any=false
+let numerosPrimos:Array<number>=[23,447,345]
+
+let nombres:Array<string>=["ana","maria","jose"]
+for(let i=0;<nombres>)
---
+++ script.js
@@ -0,0 +1,65 @@
+//EJERCICIO 1//
+/*
+function suma(a,b){
+return a+b;
+}
+a=2
+b=3
+console.log("El Resultado es " +  (a+b))
+*/
+
+//EJERCICIO 2//
+/*
+function numero(x){
+    if (x%2 == 0)
+    console.log("Es par")
+
+    else
+    console.log("No es par")
+}
+
+numero(66)
+*/
+
+//EJERCICIO 3//
+/*
+function ingresoBar(value){
+    if(value >= 18) {
+    console.log("Puede ingresar al Bar");
+}
+    else {
+    console.log("No puede Ingresar");
+}
+}
+    
+
+ingresoBar(15)
+*/
+
+//EJERCICIO 4//
+/*
+let luckyNumbers = [23,56,89,12,90,34,65,22,11,77]
+console.log("Lucky Numbers: ")
+
+for(let i=0;i<10;i++){
+    console.log(luckyNumbers[i])
+}
+*/
+
+//EJERCICIO Extra#1//
+
+/*
+let luckyNumbers = [23,56,89,12,90,34,65,22,11,77]
+
+function newNumbers(){
+    console.log(luckyNumbers)
+    console.log("Lucky Numbers sorted: ")
+    luckyNumbers.sort();
+    console.log(luckyNumbers)
+}
+
+newNumbers()
+*/
+
+//EJERCICIO Extra#2//
+
